676f406bc183f3d1b3870120fc87c3a660dc9e40
erick-arakelyan <araqelyanerick@gmail.com>
2025-02-05 20:50:19 +0400

    Update fout-digit number.cpp

--- 30.01.2024/fout-digit number.cpp
+++ 30.01.2024/fout-digit number.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
 int main() {
-    int number, sum = 0;
+    int number;
     std::cout << "Enter four-digit number: ";
     std::cin >> number;
+
     if (number < 1000 || number > 9999) {
         std::cout << "Please input correct number";
         return 1;
     }
-    while (number > 0) {
-        sum += number % 10;  
-        number /= 10;       
-    }
+    
+    int thousands = number / 1000;
+    int hundreds = (number / 100) % 10;
+    int tens = (number / 10) % 10;
+    int ones = number % 10;
+
+    int sum = thousands + hundreds + tens + ones;
+
     std::cout << "Sum of elements: " << sum;
     return 0;
 }
